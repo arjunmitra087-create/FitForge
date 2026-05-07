769ae0d5808890a9f32fe2853bda25bf10d538d2
Arjun Mitra <arjunmitra087@gmail.com>
2026-05-07 18:09:05 +0530

    fix flask import

--- workout_app.py
+++ workout_app.py
@@ -8,7 +8,7 @@ import json
 import os
 import sqlite3
 from datetime import datetime, timedelta
-from flask import send_from_directory
+from flask import Flask, render_template, request, redirect, url_for, jsonify, send_from_directory
 
 app = Flask(__name__)
 @app.route('/manifest.json')
